a9567120c5b80de9951a1f477a2f7e2fead57cf8
Грачья Дарбинян <gracadarbinan@MacBook-Pro-Graca.local>
2021-08-31 15:32:16 +0300

    module2-task2

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -8,55 +8,19 @@ const FILE_NAME = `mocks.json`;
 const MOCK_COUNT_RESTRICT = 1000;
 const MAX_MOUNTH_DIFF = 3;
 
-const TITLES = [
-  `Ёлки. История деревьев`,
-  `Как перестать беспокоиться и начать жить`,
-  `Как достигнуть успеха не вставая с кресла`,
-  `Обзор новейшего смартфона`,
-  `Лучшие рок-музыканты 20-века`,
-  `Как начать программировать`,
-  `Учим HTML и CSS`,
-  `Что такое золотое сечение`,
-  `Как собрать камни бесконечности`,
-  `Борьба с прокрастинацией`,
-  `Рок — это протест`,
-  `Самый лучший музыкальный альбом этого года`,
-];
+const FILE_ANNOUNCES_PATH = `./data/announces.txt`;
+const FILE_TITLES_PATH = `./data/titles.txt`;
+const FILE_CATEGORIES_PATH = `./data/categories.txt`;
 
-const ANNOUNCES = [
-  `Ёлки — это не просто красивое дерево. Это прочная древесина.`,
-  `Первая большая ёлка была установлена только в 1938 году.`,
-  `Вы можете достичь всего. Стоит только немного постараться и запастись книгами.`,
-  `Этот смартфон — настоящая находка. Большой и яркий экран, мощнейший процессор — всё это в небольшом гаджете.`,
-  `Золотое сечение — соотношение двух величин, гармоническая пропорция.`,
-  `Собрать камни бесконечности легко, если вы прирожденный герой.`,
-  `Освоить вёрстку несложно. Возьмите книгу новую книгу и закрепите все упражнения на практике.`,
-  `Бороться с прокрастинацией несложно. Просто действуйте. Маленькими шагами.`,
-  `Программировать не настолько сложно, как об этом говорят.`,
-  `Простые ежедневные упражнения помогут достичь успеха.`,
-  `Это один из лучших рок-музыкантов.`,
-  `Он написал больше 30 хитов.`,
-  `Из под его пера вышло 8 платиновых альбомов.`,
-  `Процессор заслуживает особого внимания. Он обязательно понравится геймерам со стажем.`,
-  `Рок-музыка всегда ассоциировалась с протестами. Так ли это на самом деле?`,
-  `Достичь успеха помогут ежедневные повторения.`,
-  `Помните, небольшое количество ежедневных упражнений лучше, чем один раз, но много.`,
-  `Как начать действовать? Для начала просто соберитесь.`,
-  `Игры и программирование разные вещи. Не стоит идти в программисты, если вам нравятся только игры.`,
-  `Альбом стал настоящим открытием года. Мощные гитарные рифы и скоростные соло-партии не дадут заскучать.`,
-];
-
-const CATEGORIES = [
-  `Деревья`,
-  `За жизнь`,
-  `Без рамки`,
-  `Разное`,
-  `IT`,
-  `Музыка`,
-  `Кино`,
-  `Программирование`,
-  `Железо`,
-];
+const readContent = async (path) => {
+  try {
+    const content = await fs.readFile(path, `utf-8`);
+    return content.trim().split(`\n`);
+  } catch (err) {
+    console.log(chalk.red(err));
+    return [];
+  }
+};
 
 const getRandFromArr = (arr) => arr[getRandomInt(0, arr.length - 1)];
 
@@ -70,7 +34,7 @@ const getDate = () => {
   return randomDate.toLocaleString();
 };
 
-const getAnnounce = () => {
+const getAnnounce = (announces) => {
   const MAX_ANNOUNCE_RESTRICT = 5;
   const MIN_ANNOUNCE_RESTRICT = 0;
   const randomTextCount = getRandomInt(
@@ -79,32 +43,36 @@ const getAnnounce = () => {
   );
   return [...Array(randomTextCount)]
     .fill(``)
-    .map(() => getRandFromArr(ANNOUNCES))
+    .map(() => getRandFromArr(announces))
     .join(` `);
 };
 
-const getFullText = () => {
-  const randomCount = getRandomInt(1, ANNOUNCES.length - 1);
+const getFullText = (announces) => {
+  const randomCount = getRandomInt(1, announces.length - 1);
   return Array(randomCount)
     .fill(``)
-    .map(() => getRandFromArr(ANNOUNCES))
+    .map(() => getRandFromArr(announces))
     .join(` `);
 };
 
-const generateOffers = (count) =>
+const generateOffers = (count, titles, categories, announces) =>
   Array(count)
     .fill({})
     .map(() => ({
-      title: getRandFromArr(TITLES),
+      title: getRandFromArr(titles),
       createdDate: getDate(),
-      announce: getAnnounce(),
-      fullText: getFullText(),
-      category: getRandFromArr(CATEGORIES),
+      announce: getAnnounce(announces),
+      fullText: getFullText(announces),
+      category: getRandFromArr(categories),
     }));
 
 module.exports = {
   name: `--generate`,
   async run(args) {
+    const titles = await readContent(FILE_TITLES_PATH);
+    const categories = await readContent(FILE_CATEGORIES_PATH);
+    const announces = await readContent(FILE_ANNOUNCES_PATH);
+
     const [count] = args;
     const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
     if (countOffer > MOCK_COUNT_RESTRICT) {
@@ -112,7 +80,7 @@ module.exports = {
       return;
     }
 
-    const content = JSON.stringify(generateOffers(countOffer));
+    const content = JSON.stringify(generateOffers(countOffer, titles, categories, announces));
 
     try {
       await fs.writeFile(FILE_NAME, content);
